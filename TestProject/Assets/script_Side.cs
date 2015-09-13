0f089ea059639e36902f4db8ee019e7ae9913150
dilip <dilip@thinqbot.com>
2015-09-13 19:31:32 +0530

    changed speed 5 to 3

--- TestProject/Assets/script_Side.cs
+++ TestProject/Assets/script_Side.cs
@@ -11,6 +11,6 @@ public class script_Side : MonoBehaviour {
 	// Update is called once per frame
 	void Update () 
     {
-        transform.Rotate(Vector3.right, 5);
+        transform.Rotate(Vector3.right, 3);
 	}
 }
